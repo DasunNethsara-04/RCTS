28c0eefb6b9813464f9bcc101263b61f07a6e364
Dasun <techsaralk.pro@gmail.com>
2024-04-20 21:21:23 +0530

    db connector added

---
+++ Connection/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "rcts";
+
+$conn = new mysqli($host, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
