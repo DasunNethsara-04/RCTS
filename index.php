973565d308d97b715d8971891b16cf23579d2bd1
Dasun <techsaralk.pro@gmail.com>
2024-04-20 17:13:00 +0530

    Login UI finished

--- index.php
+++ index.php
@@ -66,20 +66,14 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <div class="navbar-nav ms-auto p-4 p-lg-0">
-        <a href="index.html" class="nav-item nav-link active">Home</a>
-        <a href="about.html" class="nav-item nav-link">About</a>
-        <a href="courses.html" class="nav-item nav-link">Courses</a>
-        <div class="nav-item dropdown">
-          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-          <div class="dropdown-menu fade-down m-0">
-            <a href="team.html" class="dropdown-item">Our Team</a>
-            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-            <a href="404.html" class="dropdown-item">404 Page</a>
-          </div>
-        </div>
-        <a href="contact.html" class="nav-item nav-link">Contact</a>
+        <a href="index.php" class="nav-item nav-link active">Home</a>
+        <a href="#news" class="nav-item nav-link">News and Events</a>
+        <a href="#subjects" class="nav-item nav-link">Subjects</a>
+        <a href="#staff" class="nav-item nav-link">Staff</a>
+        <a href="#" class="nav-item nav-link">Gallery</a>
+        <a href="#" class="nav-item nav-link">Downloads</a>
       </div>
-      <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i
+      <a href="./login.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Login<i
           class="fa fa-arrow-right ms-3"></i></a>
     </div>
   </nav>
@@ -205,70 +199,14 @@
   </div>
   <!-- Carousel End -->
 
-  <!-- Categories Start -->
-  <div class="container-xxl py-5 category">
+  <!-- News Start -->
+  <div class="container-xxl py-5" id="news">
     <div class="container">
       <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
         <h6 class="section-title bg-white text-center text-primary px-3">
-          Categories
+          News
         </h6>
-        <h1 class="mb-5">Courses Categories</h1>
-      </div>
-      <div class="row g-3">
-        <div class="col-lg-7 col-md-6">
-          <div class="row g-3">
-            <div class="col-lg-12 col-md-12 wow zoomIn" data-wow-delay="0.1s">
-              <a class="position-relative d-block overflow-hidden" href="">
-                <img class="img-fluid" src="./assets/img/cat-1.jpg" alt="" />
-                <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px">
-                  <h5 class="m-0">Web Design</h5>
-                  <small class="text-primary">49 Courses</small>
-                </div>
-              </a>
-            </div>
-            <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.3s">
-              <a class="position-relative d-block overflow-hidden" href="">
-                <img class="img-fluid" src="./assets/img/cat-2.jpg" alt="" />
-                <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px">
-                  <h5 class="m-0">Graphic Design</h5>
-                  <small class="text-primary">49 Courses</small>
-                </div>
-              </a>
-            </div>
-            <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.5s">
-              <a class="position-relative d-block overflow-hidden" href="">
-                <img class="img-fluid" src="./assets/img/cat-3.jpg" alt="" />
-                <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px">
-                  <h5 class="m-0">Video Editing</h5>
-                  <small class="text-primary">49 Courses</small>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-5 col-md-6 wow zoomIn" data-wow-delay="0.7s" style="min-height: 350px">
-          <a class="position-relative d-block h-100 overflow-hidden" href="">
-            <img class="img-fluid position-absolute w-100 h-100" src="./assets/img/cat-4.jpg" alt=""
-              style="object-fit: cover" />
-            <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px">
-              <h5 class="m-0">Online Marketing</h5>
-              <small class="text-primary">49 Courses</small>
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Categories Start -->
-
-  <!-- Courses Start -->
-  <div class="container-xxl py-5">
-    <div class="container">
-      <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-        <h6 class="section-title bg-white text-center text-primary px-3">
-          Courses
-        </h6>
-        <h1 class="mb-5">Popular Courses</h1>
+        <h1 class="mb-5">News & Events</h1>
       </div>
       <div class="row g-4 justify-content-center">
         <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -277,32 +215,19 @@
               <img class="img-fluid" src="./assets/img/course-1.jpg" alt="" />
               <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
                 <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                  style="border-radius: 30px 0 0 30px">Read More</a>
-                <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0">Join
-                  Now</a>
+                  style="border-radius: 20px">Read More</a>
               </div>
             </div>
             <div class="text-center p-4 pb-0">
-              <h3 class="mb-0">$149.00</h3>
-              <div class="mb-3">
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small>(123)</small>
-              </div>
               <h5 class="mb-4">
                 Web Design & Development Course for Beginners
               </h5>
             </div>
             <div class="d-flex border-top">
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
-                Doe</small>
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49
-                Hrs</small>
               <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
                 Students</small>
+              <small class="flex-fill text-center py-2"><i
+                  class="fa fa-calendar text-primary me-2"></i>02.04.2022</small>
             </div>
           </div>
         </div>
@@ -312,32 +237,19 @@
               <img class="img-fluid" src="./assets/img/course-2.jpg" alt="" />
               <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
                 <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                  style="border-radius: 30px 0 0 30px">Read More</a>
-                <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0">Join
-                  Now</a>
+                  style="border-radius: 20px">Read More</a>
               </div>
             </div>
             <div class="text-center p-4 pb-0">
-              <h3 class="mb-0">$149.00</h3>
-              <div class="mb-3">
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small>(123)</small>
-              </div>
               <h5 class="mb-4">
                 Web Design & Development Course for Beginners
               </h5>
             </div>
             <div class="d-flex border-top">
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
-                Doe</small>
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49
-                Hrs</small>
               <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
                 Students</small>
+              <small class="flex-fill text-center py-2"><i
+                  class="fa fa-calendar text-primary me-2"></i>02.04.2022</small>
             </div>
           </div>
         </div>
@@ -347,48 +259,105 @@
               <img class="img-fluid" src="./assets/img/course-3.jpg" alt="" />
               <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
                 <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end"
-                  style="border-radius: 30px 0 0 30px">Read More</a>
-                <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0">Join
-                  Now</a>
+                  style="border-radius: 20px">Read More</a>
               </div>
             </div>
             <div class="text-center p-4 pb-0">
-              <h3 class="mb-0">$149.00</h3>
-              <div class="mb-3">
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small class="fa fa-star text-primary"></small>
-                <small>(123)</small>
-              </div>
               <h5 class="mb-4">
                 Web Design & Development Course for Beginners
               </h5>
             </div>
             <div class="d-flex border-top">
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
-                Doe</small>
-              <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49
-                Hrs</small>
               <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30
                 Students</small>
+              <small class="flex-fill text-center py-2"><i
+                  class="fa fa-calendar text-primary me-2"></i>02.04.2022</small>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <!-- Courses End -->
+  <!-- News End -->
 
-  <!-- Team Start -->
-  <div class="container-xxl py-5">
+  <!-- Subjects Start -->
+  <!-- recomended image resolusion: 600x400 -->
+  <div class="container-xxl py-5" id="subjects">
     <div class="container">
       <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
         <h6 class="section-title bg-white text-center text-primary px-3">
-          Instructors
+          Subjects
         </h6>
-        <h1 class="mb-5">Expert Instructors</h1>
+        <h1 class="mb-5">Our Subjects</h1>
+      </div>
+      <div class="row g-4 justify-content-center">
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div class="course-item bg-light">
+            <div class="position-relative overflow-hidden">
+              <img class="img-fluid" src="./assets/img/course-1.jpg" alt="" />
+            </div>
+            <div class="text-center p-4 pb-0">
+              <h3 class="mb-0">ET</h3>
+              <h5 class="mb-4">
+                Engineering Technology<br />
+              </h5>
+            </div>
+            <div class="d-flex border-top">
+              <!-- Teachers count -->
+              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
+                Doe</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div class="course-item bg-light">
+            <div class="position-relative overflow-hidden">
+              <img class="img-fluid" src="./assets/img/course-2.jpg" alt="" />
+            </div>
+            <div class="text-center p-4 pb-0">
+              <h3 class="mb-0">SFT</h3>
+              <h5 class="mb-4">
+                Science For Technology<br />
+              </h5>
+            </div>
+            <div class="d-flex border-top">
+              <!-- Teachers count -->
+              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
+                Doe</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div class="course-item bg-light">
+            <div class="position-relative overflow-hidden">
+              <img class="img-fluid" src="./assets/img/course-3.jpg" alt="" />
+            </div>
+            <div class="text-center p-4 pb-0">
+              <h3 class="mb-0">ICT</h3>
+              <h5 class="mb-4">
+                Information and Communication Technology
+              </h5>
+            </div>
+            <div class="d-flex border-top">
+              <!-- Teachers count -->
+              <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John
+                Doe</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Subjects End -->
+
+  <!-- Staff Member Start -->
+  <div class="container-xxl py-5" id="staff">
+    <div class="container">
+      <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+        <h6 class="section-title bg-white text-center text-primary px-3">
+          Staff
+        </h6>
+        <h1 class="mb-5">Academic Staff</h1>
       </div>
       <div class="row g-4">
         <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -496,29 +465,6 @@
             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <h4 class="text-white mb-3">Gallery</h4>
-          <div class="row g-2 pt-2">
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-1.jpg" alt="" />
-            </div>
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-2.jpg" alt="" />
-            </div>
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-3.jpg" alt="" />
-            </div>
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-2.jpg" alt="" />
-            </div>
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-3.jpg" alt="" />
-            </div>
-            <div class="col-4">
-              <img class="img-fluid bg-light p-1" src="./assets/img/course-1.jpg" alt="" />
-            </div>
           </div>
         </div>
         <div class="col-lg-3 col-md-6">
