4558ac1837047ae4cc8c03b26835d59008aee3a2
Dasun <techsaralk.pro@gmail.com>
2024-04-21 14:34:16 +0530

    Add User ui finished

--- Pages/SuperUser/add-user.php
+++ Pages/SuperUser/add-user.php
@@ -3,24 +3,56 @@
     <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
         <form method="post">
             <div class="mb-3">
-                <label for="exampleInputName" class="form-label">Your Name</label>
-                <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp"
-                    placeholder="Enter Name" name="name" required />
-                <small id="nameHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name" required />
             </div>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Enter email" name="email" required />
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                    else.</small>
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email" required />
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <div class="input-group mb-3">
+                    <input type="text" name="new_pwd" class="form-control" id="passInput" required
+                        placeholder="Enter Password">
+                    <button class="btn btn-secondary" id="gBTN">Random</button>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="user_role" class="form-label">Register as:</label>
+                <select name="user_role_id" id="user_role" class="form-select">
+                    <?php
+                    include ("../../Connection/db.php");
+                    $sql = "SELECT * FROM user_role_tbl WHERE status=1 AND NOT user_role='Super User'";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value=" . $row['user_role_id'] . ">" . $row['user_role'] . "</option>";
+                    }
+                    ?>
+                </select>
             </div>
             <button type="submit" class="btn btn-outline-success">Submit</button>
         </form>
     </div>
 </div>
-
-<!-- set page title -->
 <script>
+    // set page title
     $(document).prop('title', 'Add User - RCTS');
-</script>
+
+    // generate random password
+    var gBTN = document.getElementById('gBTN');
+    gBTN.addEventListener('click', function (e) {
+        e.preventDefault();
+        makePass(5)
+    });
+
+    function makePass(length) {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) { result += characters.charAt(Math.floor(Math.random() * charactersLength)); counter += 1; }
+        passInput.value = result;
+    } </script>
