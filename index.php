af2a453e3906abd175a311dbcd62fdd89d7bef0c
Dasun <techsaralk.pro@gmail.com>
2024-04-21 16:04:40 +0530

    made changes for Super User page and Index page

--- index.php
+++ index.php
@@ -89,9 +89,8 @@ $conn->close();
         <a href="#staff" class="nav-item nav-link">Staff</a>
         <a href="#" class="nav-item nav-link">Gallery</a>
         <a href="#" class="nav-item nav-link">Downloads</a>
+        <a href="./login.php" class="nav-item nav-link">Login</a>
       </div>
-      <a href="./login.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Login<i
-          class="fa fa-arrow-right ms-3"></i></a>
     </div>
   </nav>
   <!-- Navbar End -->
@@ -118,11 +117,8 @@ $conn->close();
                     1876. The founder of school was the Wesleyan Missionary
                     George Bough. The first principal of the school was Rev
                     Samuel Langdon. In 1882, it was renamed Richmond College. -->
-                  Richmond College Technology Society is an organization that
-                  is focused on exploring and promoting technology in various
-                  forms within the school community. Our purpose is to provide
-                  a platform for students to collaborate, learn, and gain
-                  hands-on experience with cutting-edge tools and software.
+                  Welcome to Richmond College Tech Society! Dive into collaborative learning and hands-on tech
+                  experiences with us. Let's innovate together!
                 </p>
                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
                 <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
@@ -145,11 +141,8 @@ $conn->close();
                   Richmond College Technology Society
                 </h1>
                 <p class="fs-5 text-white mb-4 pb-2">
-                  Richmond College Technology Society is an organization that
-                  is focused on exploring and promoting technology in various
-                  forms within the school community. Our purpose is to provide
-                  a platform for students to collaborate, learn, and gain
-                  hands-on experience with cutting-edge tools and software.
+                  Welcome to Richmond College Tech Society! Dive into collaborative learning and hands-on tech
+                  experiences with us. Let's innovate together!
                 </p>
                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
                 <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
@@ -172,11 +165,8 @@ $conn->close();
                   Richmond College Technology Society
                 </h1>
                 <p class="fs-5 text-white mb-4 pb-2">
-                  Richmond College Technology Society is an organization that
-                  is focused on exploring and promoting technology in various
-                  forms within the school community. Our purpose is to provide
-                  a platform for students to collaborate, learn, and gain
-                  hands-on experience with cutting-edge tools and software.
+                  Welcome to Richmond College Tech Society! Dive into collaborative learning and hands-on tech
+                  experiences with us. Let's innovate together!
                 </p>
                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
                 <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
@@ -199,11 +189,8 @@ $conn->close();
                   Richmond College Technology Society
                 </h1>
                 <p class="fs-5 text-white mb-4 pb-2">
-                  Richmond College Technology Society is an organization that
-                  is focused on exploring and promoting technology in various
-                  forms within the school community. Our purpose is to provide
-                  a platform for students to collaborate, learn, and gain
-                  hands-on experience with cutting-edge tools and software.
+                  Welcome to Richmond College Tech Society! Dive into collaborative learning and hands-on tech
+                  experiences with us. Let's innovate together!
                 </p>
                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
                 <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
@@ -453,6 +440,96 @@ $conn->close();
     </div>
   </div>
   <!-- Team End -->
+
+  <!-- Contact Start -->
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+        <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
+        <h1 class="mb-5">Contact For Any Query</h1>
+      </div>
+      <div class="row g-4">
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+          <h5>Get In Touch</h5>
+          <p class="mb-4">Have a question, comment, or inquiry? We're here to help! Get in touch with us through our
+            contact form, email, or phone number. Our dedicated team is ready to assist you with any queries or concerns
+            you may have. Feel free to reach out anytime – we're committed to providing exceptional support to our
+            valued visitors</p>
+          <div class="d-flex align-items-center mb-3">
+            <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+              style="width: 50px; height: 50px;">
+              <i class="fa fa-map-marker-alt text-white"></i>
+            </div>
+            <div class="ms-3">
+              <h5 class="text-primary">Address</h5>
+              <p class="mb-0">Richmond Hill Rd, Galle 80000, Sri Lanka</p>
+            </div>
+          </div>
+          <div class="d-flex align-items-center mb-3">
+            <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+              style="width: 50px; height: 50px;">
+              <i class="fa fa-phone-alt text-white"></i>
+            </div>
+            <div class="ms-3">
+              <h5 class="text-primary">Mobile</h5>
+              <p class="mb-0">+94 76 055 5678</p>
+            </div>
+          </div>
+          <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary"
+              style="width: 50px; height: 50px;">
+              <i class="fa fa-envelope-open text-white"></i>
+            </div>
+            <div class="ms-3">
+              <h5 class="text-primary">Email</h5>
+              <p class="mb-0">rctsenovus@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+          <iframe class="position-relative rounded w-100 h-100"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.5788732854317!2d80.20548092701924!3d6.052363462013656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae173ef4cc6b88d%3A0xd9c5ef5a5f92100b!2sRichmond%20College!5e0!3m2!1sen!2ssg!4v1687810154395!5m2!1sen!2ssg"
+            frameborder="0" style="min-height: 300px; border:0;" allowfullscreen="" aria-hidden="false"
+            tabindex="0"></iframe>
+        </div>
+        <div class="col-lg-4 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
+          <form>
+            <div class="row g-3">
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="name" placeholder="Your Name">
+                  <label for="name">Your Name</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="email" class="form-control" id="email" placeholder="Your Email">
+                  <label for="email">Your Email</label>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="subject" placeholder="Subject">
+                  <label for="subject">Subject</label>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-floating">
+                  <textarea class="form-control" placeholder="Leave a message here" id="message"
+                    style="height: 150px"></textarea>
+                  <label for="message">Message</label>
+                </div>
+              </div>
+              <div class="col-12">
+                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Contact End -->
 
   <!-- Footer Start -->
   <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
