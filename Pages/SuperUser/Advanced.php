2a82e5ae925e71c1774b75e58cb199de43f795ec
Dasun <techsaralk.pro@gmail.com>
2024-04-21 14:23:32 +0530

    Advanced settings page added

---
+++ Pages/SuperUser/Advanced.php
@@ -0,0 +1,7 @@
+<div class="container mt-5">
+    <h2>Advanced Settings</h2>
+</div>
+
+<script>
+    $(document).prop('title', 'Advanced Settings - RCTS');
+</script>
--- Pages/SuperUser/SuperUser.php
+++ Pages/SuperUser/SuperUser.php
@@ -73,7 +73,7 @@ if (!($_SESSION['email']) || $_SESSION['user_role'] != "Super User") {
         <div class="sb-sidenav-menu">
           <div class="nav">
             <div class="sb-sidenav-menu-heading">Core</div>
-            <a class="nav-link" href="./SuperUser.php?page=page1">
+            <a class="nav-link" href="./SuperUser.php?page=dashboard">
               <div class="sb-nav-link-icon">
                 <i class="fas fa-tachometer-alt"></i>
               </div>
@@ -92,54 +92,17 @@ if (!($_SESSION['email']) || $_SESSION['user_role'] != "Super User") {
             </a>
             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
               <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="./SuperUser.php?page=page2">Add User</a>
-                <a class="nav-link" href="./SuperUser.php?page=page3">Show Users</a>
+                <a class="nav-link" href="./SuperUser.php?page=add-user">Add User</a>
+                <a class="nav-link" href="./SuperUser.php?page=show-users">Show Users</a>
               </nav>
             </div>
-            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
-              aria-expanded="false" aria-controls="collapsePages">
+
+            <a class="nav-link" href="./SuperUser.php?page=advanced">
               <div class="sb-nav-link-icon">
-                <i class="fas fa-book-open"></i>
+                <i class="fa-solid fa-shield-halved"></i>
               </div>
-              Pages
-              <div class="sb-sidenav-collapse-arrow">
-                <i class="fas fa-angle-down"></i>
-              </div>
+              Advanced
             </a>
-            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth"
-                  aria-expanded="false" aria-controls="pagesCollapseAuth">
-                  Authentication
-                  <div class="sb-sidenav-collapse-arrow">
-                    <i class="fas fa-angle-down"></i>
-                  </div>
-                </a>
-                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-                  data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="login.html">Login</a>
-                    <a class="nav-link" href="register.html">Register</a>
-                    <a class="nav-link" href="password.html">Forgot Password</a>
-                  </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError"
-                  aria-expanded="false" aria-controls="pagesCollapseError">
-                  Error
-                  <div class="sb-sidenav-collapse-arrow">
-                    <i class="fas fa-angle-down"></i>
-                  </div>
-                </a>
-                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-                  data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="401.html">401 Page</a>
-                    <a class="nav-link" href="404.html">404 Page</a>
-                    <a class="nav-link" href="500.html">500 Page</a>
-                  </nav>
-                </div>
-              </nav>
-            </div>
           </div>
         </div>
         <div class="sb-sidenav-footer">
@@ -154,14 +117,17 @@ if (!($_SESSION['email']) || $_SESSION['user_role'] != "Super User") {
         if (isset($_GET['page'])) {
           $page = $_GET['page'];
           switch ($page) {
-            case 'page1':
+            case 'dashboard':
               include ("Dashboard.php");
               break;
-            case 'page2':
+            case 'add-user':
               include ("add-user.php");
               break;
-            case 'page3':
+            case 'show-users':
               include ("show-users.php");
+              break;
+            case 'advanced':
+              include ("Advanced.php");
               break;
             default:
               include ("Dashboard.php");
@@ -177,11 +143,6 @@ if (!($_SESSION['email']) || $_SESSION['user_role'] != "Super User") {
         <div class="container-fluid px-4">
           <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted">Copyright &copy; Dasun Nethsara</div>
-            <div>
-              <a href="#">Privacy Policy</a>
-              &middot;
-              <a href="#">Terms &amp; Conditions</a>
-            </div>
           </div>
         </div>
       </footer>
